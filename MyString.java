2d4d9488f135624f91b07a89adf95d2ae48fdbb3
CodyBagwandeen <cbagwandeen@stuy.edu>
2018-11-21 11:47:08 -0500

    didnt save, still send help

--- MyString.java
+++ MyString.java
@@ -13,9 +13,24 @@ public class  MyString implements CharSequence {
   private String toString() {
     String output = "";
     for ( int i =0; i < length(); i++) {
-      output += data[i];
+      output += charAt(i);
     }
     return output;
+  }
+
+  private char charAt( int index) {
+    if ( index >= length() || index < 0)
+    throw new IndexOutOfBoundsException ;
+    return data[index];
+  }
+
+  private CharSequence subsequence( int start, int end) {
+    if ( start > end || start < 0 || end < 0)
+    throw new IndexOutOfBoundsException;
+    new
+    for( int i= start; i < end; i++) {
+
+    }
   }
 
 }
